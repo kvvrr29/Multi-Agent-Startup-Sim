efd9d2c025a9822f9dc653ea3b544f757e98b5c7
Chandranil Bakshi <chandranilbakshi@gmail.com>
2026-07-19 23:38:43 +0530

    refactor(store): remove the unused storage adapter

--- src/store/persistence.js
+++ src/store/persistence.js
@@ -1,8 +1,1 @@
-export const getBrowserStorage = () => {
-  if (typeof window === 'undefined' || !window.localStorage) {
-    return { getItem: () => null, setItem: () => {}, removeItem: () => {} };
-  }
-  return window.localStorage;
-};
-
 export const cloneSerializable = (value) => JSON.parse(JSON.stringify(value));
